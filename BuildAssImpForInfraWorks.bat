49c3a75d0b3cea4fa5b7cec89799ea0652a7093b
Sebastian Voigt <sebastian.voigt@autodesk.com>
2016-04-20 11:48:36 +0200

    - Added adjusted solution and project file - Adjusted bat, so that IW uses now now 2015 workspace - adjusted git ignore list, so that it doesn't filters solution and project files anymore

--- BuildAssImpForInfraWorks.bat
+++ BuildAssImpForInfraWorks.bat
@@ -1,11 +1,11 @@
 @echo off
 Rem Builds required AssImp dlls for InfraWorks
 Rem
-Rem  VisualStudio-Version: VS2012
+Rem  VisualStudio-Version: VS2015
 Rem  Solution-Configs:     debug-noboost-dll, release-noboost-dll
 Rem  Project:              assimp (assimpview etc. are not required)
 
-set solutionFile=workspaces\vc114Assimp311\assimp.sln
+set solutionFile=workspaces\vc14Assimp311\assimp.sln
 
 call MSBuild.exe %solutionFile% /property:Configuration=debug-noboost-dll /property:Platform=x64 /target:assimp
 if errorlevel 1 (
